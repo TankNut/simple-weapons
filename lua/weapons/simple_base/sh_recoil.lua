0f07fa998eb2d582c13f990c5e04c0e5c36e578f
TankNut <m.vlasblom@live.nl>
2022-08-03 02:52:55 +0200

    Allow recoil to function outside of prediction

--- lua/weapons/simple_base/sh_recoil.lua
+++ lua/weapons/simple_base/sh_recoil.lua
@@ -3,13 +3,17 @@ AddCSLuaFile()
 simple_weapons.Include("Convars")
 
 function SWEP:ApplyRecoil(ply)
-	local command = ply:GetCurrentCommand()
+	local seed = 0
+
+	if GetPredictionPlayer() == ply then
+		seed = ply:GetCurrentCommand():CommandNumber()
+	end
 
 	local recoil = self.Primary.Recoil
 	local mult = self:GetRecoilMultiplier()
 
-	local pitch = -util.SharedRandom(self:EntIndex() .. command:CommandNumber() .. "1", recoil.MinAng.p, recoil.MaxAng.p) * mult
-	local yaw = util.SharedRandom(self:EntIndex() .. command:CommandNumber() .. "2", recoil.MinAng.y, recoil.MaxAng.y) * mult
+	local pitch = -util.SharedRandom(self:EntIndex() .. seed .. "1", recoil.MinAng.p, recoil.MaxAng.p) * mult
+	local yaw = util.SharedRandom(self:EntIndex() .. seed .. "2", recoil.MinAng.y, recoil.MaxAng.y) * mult
 
 	if game.SinglePlayer() or (CLIENT and IsFirstTimePredicted()) then
 		ply:SetEyeAngles(ply:EyeAngles() + Angle(pitch, yaw, 0) * recoil.Punch)
